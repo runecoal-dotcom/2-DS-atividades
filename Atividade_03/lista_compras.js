a65510fa19f0e58bbf3d91d2abd5e157827ba111
Rune <milaqc.bispo@gmail.com>
2026-03-12 12:14:42 -0300

    lista_compras

---
+++ Atividade_03/lista_compras.js
@@ -0,0 +1,13 @@
+let lista_compras = ["Arroz","Feijão","Açúcar","Salmão"];
+lista_compras.push("Macarrão");
+lista_compras.push("Refrigerante");
+lista_compras.push("Desinfetante");
+lista_compras.forEach(item => {
+    console.log(item);
+});
+
+let cesta_frutas = ["Maçã","Manga","Uva","Limão"];
+cesta_frutas.push("Mamão", "Pêssego");
+cesta_frutas.forEach(fruta => {
+    console.log(fruta);
+});
